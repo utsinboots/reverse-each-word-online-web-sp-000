afbeac8cb6b949a807604e4505bbb9411d2c8354
Utshant Gurung <utshantgurung@gmail.com>
2021-05-25 13:19:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,8 +4,8 @@ def reverse_each_word(sentence)
   to_array.each do |array| 
     array.reverse!()  #for each array element, this reverses every element in the same position
   end
-  string_reverse = to_array.join (" ")  #now converting the reversed array back to string
-  return string_reverse
+  return string_reverse = to_array.join (" ")  #now converting the reversed array back to string
+ # return string_reverse
 end
   
   
