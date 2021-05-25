f431fc97bba6bd0552a71070132aefea567d4307
Utshant Gurung <utshantgurung@gmail.com>
2021-05-25 12:41:23 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(sentence)
+  to_array = sentence.split(" ") #string conversion to array with spaces
+  to_array.each do |array| 
+    array.join)
+    
+    
+    string_reverse = array.reverse
+  end
+  string_reverse
+end
+  #strings.collect {|string| string.reverse}
