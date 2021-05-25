13b71b1b1fb2977bfb945d15fbd350f1bfc1a297
Utshant Gurung <utshantgurung@gmail.com>
2021-05-25 13:57:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,17 @@
 require 'pry'
 def reverse_each_word(sentence)
   to_array = sentence.split(" ") #sentence (a string) converts to array with spaces
-  
-  to_array.each do |array| #using .each method
+ 
+ 
+=begin 
+#using .each method
+  to_array.each do |array| 
      array.reverse!()  #for each array element, this reverses every element in the same position
      end
   to_array.join(" ")   #returns the reversed array as a string
+=end
 
 
-=begin
   to_array.collect do |array| #using .collect method
     array.reverse!()
   end
